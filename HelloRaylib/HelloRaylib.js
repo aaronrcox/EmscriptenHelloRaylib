aaf57cb998cacdc9ad17c062ea528e50e38da37f
aaronrcox <aaronrcox@users.noreply.github.com>
2020-12-09 06:07:40 +0000

    Deploying to gh-pages from  @ 157a5e6950546b63995ab0f26bba23ef89996b75 🚀

--- HelloRaylib/HelloRaylib.js
+++ HelloRaylib/HelloRaylib.js
@@ -187,7 +187,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"filename": "/assets/arc-codes.bmp", "start": 0, "end": 196664, "audio": 0}], "remote_package_size": 196664, "package_uuid": "cfb74f7f-f0c8-48a6-9921-bf7baedfb358"});
+   loadPackage({"files": [{"filename": "/assets/.gitkeep", "start": 0, "end": 0, "audio": 0}], "remote_package_size": 0, "package_uuid": "0650c520-0743-46e7-9804-44922f0f4198"});
   
   })();
   
